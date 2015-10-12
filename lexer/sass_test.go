ad0d3b776dfaf05af7fe0a51d9bcd297e3a07b49
Drew Wells <drew.wells00@gmail.com>
2015-10-12 18:23:42 -0500

    refactor test to reveal failed parsing, add props

--- lexer/sass_test.go
+++ lexer/sass_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"testing"
 )
@@ -10,37 +11,57 @@ func init() {
 	yyErrorVerbose = true
 }
 
-func setupParser(t *testing.T, in io.Reader) bytes.Buffer {
+func setupParser(t *testing.T, in io.Reader) (bytes.Buffer, error) {
 	buf := bytes.Buffer{}
 	out = &buf
 	lexer := NewDefault(in)
 	e := yyParse(lexer)
 	if e != 0 {
-		t.Fatal("parser reported error")
+		return buf, errors.New("parser reported error")
 	}
-	return buf
+	return buf, nil
 }
 
 func TestParserSimple(t *testing.T) {
 	in := bytes.NewBufferString(`div { color: red; }`)
-	buf := setupParser(t, in)
+	buf, err := setupParser(t, in)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if e := `div{color:red;}`; e != buf.String() {
 		t.Errorf("got: %s\nwanted: %s\n", buf.String(), e)
 	}
 }
 
+func TestParserProps(t *testing.T) {
+	in := bytes.NewBufferString(`color: blue; color: red;`)
+	buf, err := setupParser(t, in)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if e := `div p{color:red;}`; e != buf.String() {
+		t.Errorf("got: %s\nwanted: %s\n", buf.String(), e)
+	}
+}
+
 func TestParserNested(t *testing.T) {
-	yyDebug = 10
 	in := bytes.NewBufferString(`div { p { color: red; } }`)
-	buf := setupParser(t, in)
+	buf, err := setupParser(t, in)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if e := `div p{color:red;}`; e != buf.String() {
 		t.Errorf("got: %s\nwanted: %s\n", buf.String(), e)
 	}
 
-	in = bytes.NewBufferString(`div { div {p { color: red; } } }`)
-	buf = setupParser(t, in)
+	in = bytes.NewBufferString(`div { div {p {color: red; } } }`)
+	buf, err = setupParser(t, in)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if e := `div div p{color:red;}`; e != buf.String() {
 		t.Errorf("got: %s\nwanted: %s\n", buf.String(), e)
