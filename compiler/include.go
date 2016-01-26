f78f57444175c56562e68cafc57334d35e016edc
Drew Wells <drew.wells00@gmail.com>
2016-01-26 10:00:48 -0600

    include arg parsing and default arg parsing

--- compiler/include.go
+++ compiler/include.go
@@ -22,8 +22,9 @@ func printInclude(ctx *Context, n ast.Node) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("include", name)
+	fmt.Println("========\ninclude", name)
 	ctx.scope = NewScope(ctx.scope)
+	fmt.Printf("% #v\n\n\n", mix.fn.Type.Params.List[0].Type)
 	mixargs := mix.fn.Type.Params.List
 	for i := range mixargs {
 		// Param passed by include
@@ -31,22 +32,25 @@ func printInclude(ctx *Context, n ast.Node) {
 		if len(params) > i {
 			param = params[i]
 		}
-		arg := mixargs[i].Type.(*ast.BasicLit).Value
-		val, ok := param.Type.(*ast.Ident)
-		if param != nil && ok {
-			fmt.Printf("var: % #v\nval: % #v\n",
-				arg,
-				val.Name,
-			)
-			ctx.scope.Set(arg, val.Name)
-		} else {
-			fmt.Printf("var: % #v\nNOVAL: % #v\n",
-				mixargs[i].Type.(*ast.BasicLit).Value,
-				param.Type,
-			)
+		key := mixargs[i].Type.(*ast.BasicLit)
+
+		switch v := param.Type.(type) {
+		case *ast.KeyValueExpr:
+			// Key args specify their argument, so use their key
+			// instead of the mixins argument for this position
+			// Params with defaults
+			key = v.Key.(*ast.BasicLit)
+			val := v.Value.(*ast.Ident)
+			ctx.scope.Set(key.Value, val.Name)
+		case *ast.Ident:
+			ctx.scope.Set(key.Value, v.Name)
+		default:
+			fmt.Printf("dropped param: % #v\n", v)
 		}
 	}
-	// ctx.typ.Set(string, interface{})
+	if len(params) > len(mixargs) {
+		fmt.Printf("dropped extra params: % #v\n", params[len(mixargs):])
+	}
 	for _, stmt := range mix.fn.Body.List {
 		ast.Walk(ctx, stmt)
 	}
