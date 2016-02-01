2009e9c975ff252da7b1dd335f5131631163908f
Drew Wells <drew.wells00@gmail.com>
2016-02-01 03:04:02 -0600

    prepare args for registration

--- compiler/call.go
+++ compiler/call.go
@@ -2,6 +2,12 @@ package compiler
 
 import (
 	"errors"
+	"log"
+	"regexp"
+	"strings"
+
+	"github.com/wellington/sass/parser"
+	"github.com/wellington/sass/token"
 
 	"github.com/wellington/sass/ast"
 	"github.com/wellington/sass/compiler/builtin"
@@ -9,16 +15,88 @@ import (
 
 var ErrNotFound = errors.New("function does not exist")
 
-type callFunc func(args []ast.Expr) (*ast.BasicLit, error)
+type CallHandler func(args []ast.Expr) (*ast.BasicLit, error)
 
-var funcs map[string]callFunc = make(map[string]callFunc)
+type call struct {
+	name string
+	args []*ast.KeyValueExpr
+	ch   CallHandler
+}
 
-func Register(name string, fn callFunc) {
-	if _, ok := funcs[name]; ok {
-		panic("already registered: " + name)
+var funcs map[string]call = make(map[string]call)
+
+var (
+	regName = regexp.MustCompile("^\\s+").FindString
+	regArgs = regexp.MustCompile("\\$\\s+").FindAllString
+	regDef  = regexp.MustCompile(":\\s+,").FindAllString
+)
+
+type desc struct {
+	err error
+	c   call
+}
+
+func (d *desc) Visit(node ast.Node) ast.Visitor {
+	switch v := node.(type) {
+	case *ast.RuleSpec:
+		for i := range v.Values {
+			ast.Walk(d, v.Values[i])
+		}
+	case *ast.GenDecl:
+		for _, spec := range v.Specs {
+			ast.Walk(d, spec)
+		}
+		return nil
+	case *ast.CallExpr:
+		d.c.name = v.Fun.(*ast.BasicLit).Value
+		for _, arg := range v.Args {
+			switch v := arg.(type) {
+			case *ast.KeyValueExpr:
+				d.c.args = append(d.c.args, v)
+			case *ast.Ident:
+				d.c.args = append(d.c.args, &ast.KeyValueExpr{
+					Key: v,
+				})
+			default:
+				log.Fatalf("failed to parse arg % #v\n", v)
+			}
+		}
+		return nil
+	case nil:
+		return nil
+	default:
+		log.Fatalf("illegal walk % #v\n", v)
 	}
+	return d
+}
 
-	funcs[name] = fn
+func walkFunctionDescription(node ast.Node) call {
+	var args []*ast.Ident
+	_ = args
+	return call{}
+}
+
+func Register(s string, ch CallHandler) {
+	name := regName(s)
+	if _, ok := funcs[name]; ok {
+		// panic("already registered: " + name)
+	}
+	args := regArgs(s, -1)
+	_ = args
+	fset := token.NewFileSet()
+	pf, err := parser.ParseFile(fset, "", s, 0)
+	if err != nil {
+		if !strings.HasSuffix(err.Error(), "expected ';', found 'EOF'") {
+			log.Fatal(err)
+		}
+	}
+	d := &desc{c: call{}}
+	// ast.Print(fset, pf.Decls[0])
+	ast.Walk(d, pf.Decls[0])
+	if d.err != nil {
+		log.Fatal("failed to parse func description", d.err)
+	}
+	funcs[name] = d.c
 }
 
 // This might not be enough
@@ -30,7 +108,10 @@ func evaluateCall(expr *ast.CallExpr) (*ast.BasicLit, error) {
 	if !ok {
 		return notfoundCall(expr), nil
 	}
-	return fn(expr.Args)
+
+	// Verify args and convert to KeyValueExpr before passing along
+
+	return fn.ch(expr.Args)
 }
 
 // there's no such thing as a failure in Sass. Resolve idents in callexpr
@@ -41,6 +122,6 @@ func notfoundCall(call *ast.CallExpr) (lit *ast.BasicLit) {
 }
 
 func init() {
-	Register("rgb", builtin.RGB)
-	Register("rgba", builtin.RGBA)
+	Register("rgb($red:0, $green:0, $blue:0)", builtin.RGB)
+	Register("rgba($red:0, $green:0, $blue:0, $alpha:0)", builtin.RGBA)
 }
