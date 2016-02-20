848ac1f8caa78c431d37bafd19846f05b8939bba
Drew Wells <drew.wells00@gmail.com>
2016-02-20 09:28:02 -0600

    add calc package

---
+++ calc/math.go
@@ -0,0 +1,47 @@
+package calc
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/wellington/sass/ast"
+	"github.com/wellington/sass/token"
+)
+
+// Resolve simple math to create a basic lit
+func Resolve(in ast.Expr) *ast.BasicLit {
+	return resolve(in)
+}
+
+func resolve(in ast.Expr) *ast.BasicLit {
+	switch v := in.(type) {
+	case *ast.BinaryExpr:
+		return binary(v)
+	case *ast.BasicLit:
+		return v
+	}
+	return nil
+}
+
+// binary takes a BinaryExpr and simplifies it to a
+func binary(in *ast.BinaryExpr) *ast.BasicLit {
+	left, right := resolve(in.X), resolve(in.Y)
+	out := &ast.BasicLit{
+		ValuePos: left.Pos(),
+		Kind:     token.STRING,
+	}
+	switch in.Op {
+	case token.ADD:
+		if left.Kind == token.INT && left.Kind == left.Kind {
+			l, _ := strconv.Atoi(left.Value)
+			r, _ := strconv.Atoi(right.Value)
+			out.Kind = token.INT
+			out.Value = strconv.Itoa(l + r)
+		} else {
+			out.Value = left.Value + right.Value
+		}
+	default:
+		log.Fatalf("unsupported: %s", in.Op)
+	}
+	return out
+}
---
+++ calc/math_test.go
@@ -0,0 +1,59 @@
+package calc
+
+import (
+	"testing"
+
+	"github.com/wellington/sass/ast"
+	"github.com/wellington/sass/token"
+)
+
+func TestBinary_simple_int(t *testing.T) {
+	bin := &ast.BinaryExpr{
+		X:  &ast.BasicLit{Kind: token.INT, Value: "1"},
+		Op: token.ADD,
+		Y:  &ast.BasicLit{Kind: token.INT, Value: "2"},
+	}
+	lit := binary(bin)
+
+	if lit.Kind != token.INT {
+		t.Fatal("unexpected kind")
+	}
+
+	if e := "3"; lit.Value != e {
+		t.Errorf("got: %s wanted: %s", lit.Value, e)
+	}
+
+}
+
+func TestBinary_simple_str(t *testing.T) {
+	bin := &ast.BinaryExpr{
+		X:  &ast.BasicLit{Kind: token.STRING, Value: "a"},
+		Op: token.ADD,
+		Y:  &ast.BasicLit{Kind: token.STRING, Value: "b"},
+	}
+	lit := binary(bin)
+
+	if lit.Kind != token.STRING {
+		t.Fatal("unexpected kind")
+	}
+
+	if e := "ab"; lit.Value != e {
+		t.Errorf("got: %s wanted: %s", lit.Value, e)
+	}
+
+	bin = &ast.BinaryExpr{
+		X:  &ast.BasicLit{Kind: token.STRING, Value: "a"},
+		Op: token.ADD,
+		Y:  &ast.BasicLit{Kind: token.INT, Value: "1"},
+	}
+	lit = binary(bin)
+
+	if lit.Kind != token.STRING {
+		t.Fatal("unexpected kind")
+	}
+
+	if e := "a1"; lit.Value != e {
+		t.Errorf("got: %s wanted: %s", lit.Value, e)
+	}
+
+}
