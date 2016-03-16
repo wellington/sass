0af62234630d17f54e3a02f2d7c849eb5d27f532
Drew Wells <drew.wells00@gmail.com>
2016-03-16 02:59:16 -0500

    typeof is handler now

--- builtin/introspect/typeof_test.go
+++ builtin/introspect/typeof_test.go
@@ -9,21 +9,23 @@ import (
 
 func TestTypeOf(t *testing.T) {
 	call := &ast.CallExpr{}
-	lit, err := typeOf(call, &ast.BasicLit{
+	x, err := typeOf(call, &ast.BasicLit{
 		Kind:  token.INT,
 		Value: "1",
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
+	lit := x.(*ast.BasicLit)
 	if e := "number"; lit.Value != e {
 		t.Errorf("got: %s wanted: %s", lit.Value, e)
 	}
 
-	lit, err = typeOf(call, &ast.BasicLit{
+	x, err = typeOf(call, &ast.BasicLit{
 		Kind:  token.STRING,
 		Value: "a",
 	})
+	lit = x.(*ast.BasicLit)
 	if err != nil {
 		t.Fatal(err)
 	}
